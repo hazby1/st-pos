742871ed2de66c042bf5b4fa5ffc1d139d9bac97
hazby1 <hanizfoto@gmail.com>
2022-10-20 17:53:29 +0700

    Membuat seeder Kategori

--- app/Database/Seeds/Kategori.php
+++ app/Database/Seeds/Kategori.php
@@ -8,6 +8,20 @@ class Kategori extends Seeder
 {
     public function run()
     {
-        //
+        // Membuat data
+        $satuan_data = [
+            ['nama_satuan' => 'Laptop'],
+            ['nama_satuan' => 'PC'],
+            ['nama_satuan' => 'Router'],
+            ['nama_satuan' => 'Printer'],
+            ['nama_satuan' => 'RAM'],
+            ['nama_satuan' => 'SSD'],
+            ['nama_satuan' => 'HDD']
+        ];
+
+        foreach ($satuan_data as $data) {
+            // Insert semua data ke tabel
+            $this->db->table('t_kategori')->insert($data);
+        }
     }
 }
