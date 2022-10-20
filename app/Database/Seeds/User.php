c9308a08d56ceb09af6e932dacb812369df3f7ee
hazby1 <hanizfoto@gmail.com>
2022-10-20 19:00:56 +0700

    seeder user uploaded

--- app/Database/Seeds/User.php
+++ app/Database/Seeds/User.php
@@ -8,6 +8,31 @@ class User extends Seeder
 {
     public function run()
     {
-        //
+        // Membuat data
+        $user_data = [
+            [
+                'nama_user' => 'Admn',
+                'email' => 'admin@admin.com',
+                'password' => '21232f297a57a5a743894a0e4a801fc3',
+                'level' => 'admin'
+            ],
+            [
+                'nama_user' => 'Kasir',
+                'email' => 'kasir@admin.com',
+                'password' => '21232f297a57a5a743894a0e4a801fc3',
+                'level' => 'kasir'
+            ],
+            [
+                'nama_user' => 'Teknisi',
+                'email' => 'teknisi@admin.com',
+                'password' => '21232f297a57a5a743894a0e4a801fc3',
+                'level' => 'teknisi'
+            ],
+        ];
+
+        foreach ($user_data as $data) {
+            // Insert semua data ke tabel
+            $this->db->table('t_user')->insert($data);
+        }
     }
 }
