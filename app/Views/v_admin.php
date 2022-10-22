f73967ba8248dac0f899e32a2f82eaef1b7c5778
hazby1 <hanizfoto@gmail.com>
2022-10-22 17:13:29 +0700

    Memperbaruai dashboard

--- app/Views/v_admin.php
+++ app/Views/v_admin.php
@@ -1,3 +1,4 @@
+<!-- Widget -->
 <div class="col-lg-3 col-6">
 
     <div class="small-box bg-info">
@@ -14,7 +15,7 @@
 
 <div class="col-lg-3 col-6">
 
-    <div class="small-box bg-success">
+    <div class="small-box bg-teal">
         <div class="inner">
             <h3>53<sup style="font-size: 50%">%</sup></h3>
             <p>Kategori</p>
@@ -28,7 +29,7 @@
 
 <div class="col-lg-3 col-6">
 
-    <div class="small-box bg-warning">
+    <div class="small-box bg-olive">
         <div class="inner">
             <h3>44</h3>
             <p>Satuan</p>
@@ -42,7 +43,7 @@
 
 <div class="col-lg-3 col-6">
 
-    <div class="small-box bg-danger">
+    <div class="small-box bg-pink">
         <div class="inner">
             <h3>65</h3>
             <p>User</p>
@@ -53,3 +54,90 @@
         <a href="#" class="small-box-footer">Lebih lanjut <i class="fas fa-arrow-circle-right"></i></a>
     </div>
 </div>
+
+<!-- end Widget -->
+
+<div class="col-md-4">
+    <!-- Info Boxes Style 2 -->
+    <div class="info-box mb-3 bg-indigo">
+        <span class="info-box-icon"><i class="fas fa-money-bill-wave"></i></span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">Pendapatan hari ini</span>
+            <span class="info-box-number">Rp5,200</span>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+</div>
+<div class="col-md-4">
+    <!-- Info Boxes Style 2 -->
+    <div class="info-box mb-3 bg-maroon">
+        <span class="info-box-icon"><i class="fas fa-money-bill-alt"></i></span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">Pendapatan bulan ini</span>
+            <span class="info-box-number">Rp5,200</span>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+</div>
+<div class="col-md-4">
+    <!-- Info Boxes Style 2 -->
+    <div class="info-box mb-3 bg-navy">
+        <span class="info-box-icon"><i class="fas fa-money-bill"></i></span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">Pendapatan tahun ini</span>
+            <span class="info-box-number">Rp5,200</span>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+</div>
+
+<div class="col md-12">
+    <canvas id="myChart" width="100%" height="30px"></canvas>
+</div>
+
+<script>
+    const ctx = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+            datasets: [{
+                label: '# Pendapatan',
+                data: [12, 19, 3, 5, 6, 3, 3, 9, 3, 4, 17, 10],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
--- app/Views/v_template.php
+++ app/Views/v_template.php
@@ -12,62 +12,16 @@
     <link rel="stylesheet" href="<?= base_url('adminlte'); ?>/plugins/fontawesome-free/css/all.min.css">
 
     <link rel="stylesheet" href="<?= base_url('adminlte'); ?>/dist/css/adminlte.min.css?v=3.2.0">
-    <script nonce="01ed98c6-901b-4dce-8364-ae7d3ec99072">
-        (function(w, d) {
-            ! function(a, e, t, r) {
-                a.zarazData = a.zarazData || {};
-                a.zarazData.executed = [];
-                a.zaraz = {
-                    deferred: [],
-                    listeners: []
-                };
-                a.zaraz.q = [];
-                a.zaraz._f = function(e) {
-                    return function() {
-                        var t = Array.prototype.slice.call(arguments);
-                        a.zaraz.q.push({
-                            m: e,
-                            a: t
-                        })
-                    }
-                };
-                for (const e of ["track", "set", "debug"]) a.zaraz[e] = a.zaraz._f(e);
-                a.zaraz.init = () => {
-                    var t = e.getElementsByTagName(r)[0],
-                        z = e.createElement(r),
-                        n = e.getElementsByTagName("title")[0];
-                    n && (a.zarazData.t = e.getElementsByTagName("title")[0].text);
-                    a.zarazData.x = Math.random();
-                    a.zarazData.w = a.screen.width;
-                    a.zarazData.h = a.screen.height;
-                    a.zarazData.j = a.innerHeight;
-                    a.zarazData.e = a.innerWidth;
-                    a.zarazData.l = a.location.href;
-                    a.zarazData.r = e.referrer;
-                    a.zarazData.k = a.screen.colorDepth;
-                    a.zarazData.n = e.characterSet;
-                    a.zarazData.o = (new Date).getTimezoneOffset();
-                    a.zarazData.q = [];
-                    for (; a.zaraz.q.length;) {
-                        const e = a.zaraz.q.shift();
-                        a.zarazData.q.push(e)
-                    }
-                    z.defer = !0;
-                    for (const e of [localStorage, sessionStorage]) Object.keys(e || {}).filter((a => a.startsWith("_zaraz_"))).forEach((t => {
-                        try {
-                            a.zarazData["z_" + t.slice(7)] = JSON.parse(e.getItem(t))
-                        } catch {
-                            a.zarazData["z_" + t.slice(7)] = e.getItem(t)
-                        }
-                    }));
-                    z.referrerPolicy = "origin";
-                    z.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(a.zarazData)));
-                    t.parentNode.insertBefore(z, t)
-                };
-                ["complete", "interactive"].includes(e.readyState) ? zaraz.init() : a.addEventListener("DOMContentLoaded", zaraz.init)
-            }(w, d, 0, "script");
-        })(window, document);
-    </script>
+
+    <script src="<?= base_url('adminlte'); ?>/plugins/jquery/jquery.min.js"></script>
+
+    <script src="<?= base_url('adminlte'); ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="<?= base_url('adminlte'); ?>/dist/js/adminlte.min.js?v=3.2.0"></script>
+
+    <!-- ChartJS -->
+    <script src="<?= base_url('adminlte'); ?>/plugins/chart.js/Chart.min.js"></script>
+    <!-- end ChartJS -->
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -203,7 +157,7 @@
                 </div>
             </div>
 
-
+            <!-- Content -->
             <div class="content">
                 <div class="container-fluid">
                     <div class="row">
@@ -220,6 +174,7 @@
 
                 </div>
             </div>
+            <!-- end Content -->
 
         </div>
 
@@ -243,13 +198,6 @@
         </footer>
     </div>
 
-
-
-    <script src="<?= base_url('adminlte'); ?>/plugins/jquery/jquery.min.js"></script>
-
-    <script src="<?= base_url('adminlte'); ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <script src="<?= base_url('adminlte'); ?>/dist/js/adminlte.min.js?v=3.2.0"></script>
 </body>
 
 </html>
